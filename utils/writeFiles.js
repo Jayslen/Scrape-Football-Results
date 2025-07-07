d35d7baa13d575c6ff5d44f2a47d5353e9ea43ec
jayslen <jayslen134@gmail.com>
2025-07-06 20:24:14 -0400

    refactor: update writefiles function to get the dir an filename by parameters

--- utils/writeFiles.js
+++ utils/writeFiles.js
@@ -1,14 +1,14 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
 
-export async function writeMatchesData ({ data }) {
+export async function writeData ({ data, dir, fileName }) {
   const { root } = path.parse(process.cwd())
 
-  const dir = data.league.replaceAll(' ', '-')
-  const leagueRound = data.matchWeek
+  // const dir = data.league.replaceAll(' ', '-')
+  // const leagueRound = data.matchWeek
   const route = path.join(root, dir)
 
-  const fileName = `/${leagueRound.replaceAll(' ', '-')}.json`
+  // const fileName = `/${leagueRound.replaceAll(' ', '-')}.json`
 
   await fs.mkdir(route, { recursive: true })
 
